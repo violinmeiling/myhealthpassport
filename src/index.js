e52fc592c54e141db3728627fe0f2e158eb53d3b
MeilingM <violinmeiling@gmail.com>
2024-09-21 13:27:14 -0400

    update

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
-import { createRoot } from 'react-dom/client';
-import { withAuthInfo } from '@propelauth/react';
+import { AuthProvider } from "@propelauth/react";
 
-function NavigationBar() {
-  // TODO: Actually implement a navigation bar
-  const YourApp = withAuthInfo((props) => {
-    // isLoggedIn and user are injected automatically from withAuthInfo
-    if (props.isLoggedIn) {
-        return <p>You are logged in as {props.user.email}</p>
-    } else {
-        return <p>You are not logged in</p>
-    }
-})
-}
-
-const domNode = document.getElementById('navigation');
-const root = createRoot(domNode);
-root.render(<NavigationBar />);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+    <AuthProvider authUrl={process.env.REACT_APP_AUTH_URL}>
+        <YourApp />
+    </AuthProvider>,
+    document.getElementById("root")
+);
