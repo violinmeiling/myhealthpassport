24a59cc3a058d6b97dc3486ceba270c78bb43c7a
MeilingM <violinmeiling@gmail.com>
2024-09-21 13:21:55 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 import { createRoot } from 'react-dom/client';
+import { withAuthInfo } from '@propelauth/react';
 
 function NavigationBar() {
   // TODO: Actually implement a navigation bar
-  return <h1>Hello from React!</h1>;
+  const YourApp = withAuthInfo((props) => {
+    // isLoggedIn and user are injected automatically from withAuthInfo
+    if (props.isLoggedIn) {
+        return <p>You are logged in as {props.user.email}</p>
+    } else {
+        return <p>You are not logged in</p>
+    }
+})
 }
 
 const domNode = document.getElementById('navigation');
